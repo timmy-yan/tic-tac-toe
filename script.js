f460d8c3857e1860b8d37d3902cf8b1113412247
Timmy Yan <79166775+timmy-yan@users.noreply.github.com>
2021-03-05 01:11:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,60 @@ container.addEventListener("click", (e) => {
     return;
   }
 });
+
+function checkWinner() {
+  // check rows and columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      (grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2]) ||
+      (grid[0][i] === grid[1][i] && grid[0][i] === grid[2][i])
+    ) {
+      return true;
+    }
+  }
+  // check diagonals
+  if (
+    (grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) ||
+    (grid[2][0] === grid[1][1] && grid[2][0] === grid[0][2])
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function displayText() {
+  document.querySelector("h2").innerHTML = "";
+
+  if (playerOne) {
+    player1Wins++;
+    return "Player One has won!";
+  } else {
+    player2Wins++;
+    return "Player Two has won!";
+  }
+}
+
+function updateScores() {
+  document.querySelector("h3").innerHTML =
+    "Player 1 Score: " +
+    player1Wins +
+    " &nbsp;|&nbsp; Player 2 Score: " +
+    player2Wins;
+}
+
+function reset() {
+  grid = [
+    [-1, -2, -3],
+    [-4, -5, -6],
+    [-7, -8, -9],
+  ];
+  playerOne = true;
+  counter = 0;
+  document.querySelector("h1").innerText = "";
+  document.querySelector("h2").innerHTML = "Player 1 your turn!";
+  document
+    .querySelectorAll(".container div")
+    .forEach((div) => (div.innerText = ""));
+
+  updateScores();
+}
